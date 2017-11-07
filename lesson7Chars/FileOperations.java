25f6bfd044f0dccd883fab1acbfedbfcefd04c97
AleksandrF <31497497+ZeusComing@users.noreply.github.com>
2017-11-07 10:47:15 +0200

    Add files via upload

--- lesson7Chars/FileOperations.java
+++ lesson7Chars/FileOperations.java
@@ -42,15 +42,9 @@ public class FileOperations {
     }
 
     public static void sortByValue(HashMap<Character, Integer> map) {
-        List entryList = new ArrayList(map.entrySet());
-        Collections.sort(entryList, new Comparator() {
-            public int compare(Object o1, Object o2) {
-                Map.Entry e1 = (Map.Entry) o1;
-                Map.Entry e2 = (Map.Entry) o2;
-                Comparable c1 = (Comparable) e2.getValue();
-                Comparable c2 = (Comparable) e1.getValue();
-                return c1.compareTo(c2);
-            }
+        List<Map.Entry<Character, Integer>> entryList = new ArrayList<>(map.entrySet());
+        Collections.sort(entryList, (mapEntry1, mapEntry2) -> {
+            return mapEntry2.getValue().compareTo(mapEntry1.getValue());
         });
         System.out.print(entryList);
 
